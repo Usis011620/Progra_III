70b767fc5a392b10d3b1c5dec09a55c791b7f9e0
Wilbert Ruth <octavioeaevab@gmail.com>
2021-11-16 21:34:06 -0800

    Terminacion CRUD

--- server.py
+++ server.py
@@ -14,6 +14,42 @@ class crud:
             print('Conectado exitosamente a la base de datos')
         else:
             print('Error al conectar a la base de datos')
+    def consultar(self):
+        try:
+            cursor = self.conexion.cursor(dictionary=True)
+            sql = "SELECT pacientes.id_Paciente, pacientes.Nombre, pacientes.Direccion, pacientes.Edad FROM pacientes"
+            cursor.execute(sql)
+            resultado = cursor.fetchall()
+            return resultado
+        except Exception as e:
+            return str(e)
+    
+    def ejecutar_consulta(self, sql, val):
+        try:
+            cursor = self.conexion.cursor()
+            cursor.execute(sql, val)
+            self.conexion.commit()
+            return "Registro procesado con exito"
+        except Exception as e:
+            return str(e)
+
+    def administrar_pacientes(self, contenido):
+        try:
+            if contenido["accion"]=="nuevo":
+                sql = "INSERT INTO pacientes (Nombre, Direccion, Edad) VALUES (%s, %s, %s)"
+                val = (contenido["Nombre"], contenido["Direccion"], contenido["Edad"])
+
+            elif contenido["accion"]=="modificar":
+                sql = "UPDATE pacientes SET Nombre=%s, Direccion=%s, Edad=%s WHERE id_Paciente=%s"
+                val = (contenido["Nombre"], contenido["Direccion"], contenido["Edad"], contenido["id_Paciente"])
+
+            elif contenido["accion"]=="eliminar":
+                sql = "DELETE FROM pacientes WHERE id_Paciente=%s"
+                val = (contenido["id_Paciente"],)
+
+            return self.ejecutar_consulta(sql, val)
+        except Exception as e:
+            return str(e)
 crud = crud()
 
 class servidorBasico(SimpleHTTPRequestHandler):
@@ -21,6 +57,12 @@ class servidorBasico(SimpleHTTPRequestHandler):
         if self.path == '/':
             self.path = '/index.html'
             return SimpleHTTPRequestHandler.do_GET(self)
+        
+        if self.path == '/consultar':
+            resp = crud.consultar()
+            self.send_response(200)
+            self.end_headers()
+            self.wfile.write(json.dumps(dict(resp=resp)).encode('utf-8'))
 
     def do_POST(self):
         if self.path == '/insertar':
@@ -29,6 +71,10 @@ class servidorBasico(SimpleHTTPRequestHandler):
             data = data.decode('utf-8')
             data = parse.unquote(data)
             data = json.loads(data)
+            resp = crud.administrar_pacientes(data)
+            self.send_response(200)
+            self.end_headers()
+            self.wfile.write(json.dumps(dict(resp=resp)).encode('utf-8'))
 
 print('Servidor iniciado en el puerto 3000')
 servidor = HTTPServer(('localhost', 3000), servidorBasico)
